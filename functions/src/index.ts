db61ffdc5ea9a77f4b0124462b542ab6620b849f
hanock <htsegaye@terpmail.umd.edu>
2023-10-30 20:36:18 -0400

    1st commit user-auth

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -17,3 +17,119 @@
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+
+export const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+export const app = admin.initializeApp();
+const db = admin.firestore();
+const cors = require("cors");
+const corsHandler = cors({ origin: true });
+
+/*
+ * Creates a new user
+ *
+ * Arguments: id: number,
+              email: string,
+              pass: Hash,
+              firstName: string,
+              lastName: string,
+              phone: number,
+              zip: number,
+              children: {
+                firstName: string,
+                lastName: string,
+                age: number,
+                birthYear: number,
+                school: string,
+              }[],
+              notifs: boolean,
+ */
+
+exports.createUser = functions
+  .region("us-east4")
+  .https.onRequest((req: any, res: any) => {
+    corsHandler(req, res, async () => {
+      const auth = admin.auth();
+      await auth
+        .verifyIdToken(req.headers.authorization.split("Bearer ")[1])
+        .then(async (decodedToken: any) => {
+          if (
+            req.body.data.id == null ||
+            req.body.data.email == null ||
+            req.body.data.pass == null ||
+            req.body.data.firstName == null ||
+            req.body.data.lastName == null ||
+            req.body.data.phone == null ||
+            req.body.data.zip == null ||
+            req.body.data.children == null ||
+            req.body.data.children.firstName == null ||
+            req.body.data.children.lastName == null ||
+            req.body.data.children.age == null ||
+            req.body.data.children.birthYear == null ||
+            req.body.data.children.school == null ||
+            req.body.data.notifs == null
+          ) {
+            throw new functions.https.HttpsError(
+              "invalid-argument",
+              "Missing arguments. Request must include email, name, and role."
+            );
+          } else if (decodedToken.role.toLowerCase() != "admin") {
+            throw new functions.https.HttpsError(
+              "permission-denied",
+              "Only an admin user can create users"
+            );
+          } else {
+            await auth
+              .createUser({
+                email: req.body.data.email,
+                password: "defaultpassword",
+              })
+              .then(async (userRecord: any) => {
+                await auth
+                  .setCustomUserClaims(userRecord.uid, {
+                    role: req.body.data.role,
+                  })
+                  .then(async () => {
+                    await db
+                      .collection("Users")
+                      .add({
+                        auth_id: userRecord.uid,
+                        email: req.body.data.email,
+                        name: req.body.data.name,
+                        type: req.body.data.role.toUpperCase(),
+                      })
+                      .then(() => {
+                        res.json({ result: "Complete" });
+                      })
+                      .catch((error) => {
+                        throw new functions.https.HttpsError(
+                          "Unknown",
+                          "Failed to add user to database"
+                        );
+                      });
+                  })
+                  .catch((error) => {
+                    throw new functions.https.HttpsError(
+                      "Unknown",
+                      "Failed to set user's role"
+                    );
+                  });
+              })
+              .catch((error) => {
+                throw new functions.https.HttpsError(
+                  "Unknown",
+                  "Failed to add user to authorization"
+                );
+              });
+          }
+        })
+        .catch((error) => {
+          throw new functions.https.HttpsError(
+            "unauthenticated",
+            "failed to authenticate request. ID token is missing or invalid."
+          );
+        });
+    });
+  });
+
