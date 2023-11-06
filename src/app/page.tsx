e1fc3a2ccc52dabaf784db19c10416883e95d4db
Chirag Jain <taruschirag@Chirags-Laptop.local>
2023-11-06 01:00:33 -0500

    finished the top half of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import styles from 'page.module.css'
+import styles from './page.module.css'
 
 const YourComponent = () => {
     return (
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-            <img src="/Top_Rectangle.svg" alt="TopRectangle" />
-            <img src="/image 4.svg" alt="Image 4" style={{ marginLeft: '10px' }} />
+        <div>
+            <div className={styles.topRectangle}>
+                <img src="/Top_Rectangle.svg" alt="TopRectangle" />
+                <img src="/image 4.svg" alt="Image 4" className={styles.logo} />
+                <div className={styles.menuContainer}>
+                <img src="/Rectangle 68.svg" alt="Menu_Rectangle"  className = {styles.menuItems} />
+                <img src="/Rectangle 69.svg" alt="Menu_Rectangle" className={styles.menuItems}/>
+                <img src="/Rectangle 70.svg" alt="Menu_Rectangle" className={styles.menuItems}/>
+            </div>
+            </div>
+
         </div>
     );
 };
