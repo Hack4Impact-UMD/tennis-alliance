00207c04d5e1c9d90f5c03cb18df6466c1f13b0d
Chirag Jain <taruschirag@Chirags-Laptop.local>
2023-11-13 03:25:47 -0500

    made UI for login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './page.module.css'
 
-const YourComponent = () => {
+const UIPage = () => {
     return (
-        <div>
+        <div className= {styles.loginContainer}>
             <div className={styles.topRectangle}>
                 <img src="/Top_Rectangle.svg" alt="TopRectangle" />
                 <img src="/image 4.svg" alt="Image 4" className={styles.logo} />
@@ -11,11 +11,16 @@ const YourComponent = () => {
                 <img src="/Rectangle 68.svg" alt="Menu_Rectangle"  className = {styles.menuItems} />
                 <img src="/Rectangle 69.svg" alt="Menu_Rectangle" className={styles.menuItems}/>
                 <img src="/Rectangle 70.svg" alt="Menu_Rectangle" className={styles.menuItems}/>
+                </div>
             </div>
+            <div className= {styles.bodyContainer}>
+            <img src="/Group 27.svg" alt="Ellipses" id="centeredImage"/>
+
+               
             </div>
 
         </div>
     );
 };
 
-export default YourComponent;
+export default UIPage;
