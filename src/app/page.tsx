d71fe36fbdf9b045d22e2ee061e36cd10352863f
Chirag Jain <taruschirag@wireless-10-105-151-16.umd.edu>
2023-11-13 15:27:49 -0500

    cant find the background image of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,104 +1,21 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import React from 'react';
+import styles from './page.module.css'
 
-const Home = () => {
+
+const ForgotPasswordPage = () => {
     return (
-        <main className={styles.main}>
-            <div className={styles.description}>
-                <p>
-                    Get started by editing&nbsp;
-                    <code className={styles.code}>src/app/page.tsx</code>
-                </p>
-                <div>
-                    <a
-                        href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        By{" "}
-                        <Image
-                            src="/vercel.svg"
-                            alt="Vercel Logo"
-                            className={styles.vercelLogo}
-                            width={100}
-                            height={24}
-                            priority
-                        />
-                    </a>
+        <div className= {styles.loginContainer}>
+            <div className={styles.topRectangle}>
+                <img src="./Rectangle 67.svg" alt="TopRectangle" />
+                <img src="./logo.svg" alt="Image 4" className={styles.logo} />
+                <div className={styles.menuContainer}>
+                <img src="./Rectangle 68.svg" alt="Menu_Rectangle"  className = {styles.menuItems} />
+                <img src="./Rectangle 69.svg" alt="Menu_Rectangle" className={styles.menuItems}/>
+                <img src="./Rectangle 70.svg" alt="Menu_Rectangle" className={styles.menuItems}/>
                 </div>
             </div>
-
-            <div className={styles.center}>
-                <Image
-                    className={styles.logo}
-                    src="/next.svg"
-                    alt="Next.js Logo"
-                    width={180}
-                    height={37}
-                    priority
-                />
-            </div>
-
-            <div className={styles.grid}>
-                <a
-                    href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className={styles.card}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2>
-                        Docs <span>-&gt;</span>
-                    </h2>
-                    <p>
-                        Find in-depth information about Next.js features and
-                        API.
-                    </p>
-                </a>
-
-                <a
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className={styles.card}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2>
-                        Learn <span>-&gt;</span>
-                    </h2>
-                    <p>
-                        Learn about Next.js in an interactive course
-                        with&nbsp;quizzes!
-                    </p>
-                </a>
-
-                <a
-                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className={styles.card}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2>
-                        Templates <span>-&gt;</span>
-                    </h2>
-                    <p>Explore the Next.js 13 playground.</p>
-                </a>
-
-                <a
-                    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className={styles.card}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2>
-                        Deploy <span>-&gt;</span>
-                    </h2>
-                    <p>
-                        Instantly deploy your Next.js site to a shareable URL
-                        with Vercel.
-                    </p>
-                </a>
-            </div>
-        </main>
+        </div>
     );
 };
 
-export default Home;
+export default ForgotPasswordPage;
