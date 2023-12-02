9e307adb02dd0a0e308ebe4821bdaebe3f699098
Anoushka <anoushka.m.arora@gmail.com>
2023-12-01 20:40:31 -0500

    filter button

--- src/pages/admin_dash.tsx
+++ src/pages/admin_dash.tsx
@@ -1,7 +1,8 @@
-'use client'
+"use client"
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "@/styles/adminDash.module.css"
+import {Checkbox} from "@nextui-org/react";
 
 
 
@@ -9,12 +10,32 @@ import styles from "@/styles/adminDash.module.css"
 
 
 const participants = [
-    { name: "John Doe", email: "johndoe@gmail.com", type: "Volunteer"}, 
-    { name: "Jane Doe", email: "janedoe@gmail.com", type: "Participant"}, 
-    { name: "Jason Doe", email: "jasondoe@gmail.com", type: "Participant"}]
+    { first_name: "John", last_name: "Doe",  email: "johndoe@gmail.com", type: "Volunteer", age: "10", town:"Jersey City"}, 
+    { first_name: "Jane", last_name: "Dame",  email: "janedoe@gmail.com", type: "Participant", age: "11", town:"Jersey City"}, 
+    { first_name: "Jason", last_name: "Doe", email: "jasondoe@gmail.com", type: "Participant", age: "15", town:"Jersey City"}]; 
+
+
 
 const Searchbar = () => {
-    const [activeSearch, setActiveSearch] = useState<{name: string,email: string, type:string }[]>([]);
+
+    const [showFilterBar, setShowFilterBar] = useState(false); 
+
+    const [activeSearch, setActiveSearch] = useState<{ first_name: string; last_name: string; email: string; type: string; age: string; town: string}[]>([]);
+    const [lastNameFilter, setLastNameFilter] = useState('');
+    const [townFilter, setTownFilter] = useState('');
+    const [ageFilter, setAgeFilter] = useState('');
+    const [participantFilter, setParticipantFilter] = useState(true);
+    const [volunteerFilter, setVolunteerFilter] = useState(true);
+
+    useEffect(() => {
+        // Set activeSearch to the full list of participants initially
+        setActiveSearch(participants);
+    }, []);
+
+    const handleFilterButtonClick = () => {
+        setShowFilterBar(!showFilterBar); // Toggle the visibility of the filter bar
+    }
+
     const handleSearch = (e:any) => {
         const searchTerm = e.target.value;
 
@@ -23,44 +44,83 @@ const Searchbar = () => {
         } else {
             // Filter participants and extract names
             const filteredParticipants = participants.filter((participant) =>
-                participant.name.toLowerCase().includes(searchTerm.toLowerCase())
+                participant.last_name.toLowerCase().includes(searchTerm.toLowerCase())
             );
 
             setActiveSearch(filteredParticipants);
         }
     }
 
+    const applyFilters = (participantsToFilter: { first_name: string; last_name: string;email: string; type: string; age: string; town: string }[]) => {
+        // Apply filters
+        const filteredParticipants = participantsToFilter.filter(
+            (participant) =>
+                participant.last_name.toLowerCase().includes(lastNameFilter.toLowerCase()) &&
+                participant.town.toLowerCase().includes(townFilter.toLowerCase()) && 
+                participant.age.toLowerCase().includes(ageFilter.toLowerCase()) && 
+                ((participantFilter && participant.type === 'Participant') || (volunteerFilter && participant.type === 'Volunteer') || (participantFilter && volunteerFilter))
+        );
+    
+        setActiveSearch(filteredParticipants);
+    }
+
+
     return (
-        <form className={styles.form}>
+        <div>
             <span className={styles.label}>All Participants</span>
-            <div className={styles.parent}>
+            <div >
                 <input className={styles.search} type="search" placeholder='Search' onChange ={(e) => handleSearch(e)}>
                 </input>
+                <button onClick={handleFilterButtonClick} className = {styles.filter}> Filter </button>
             </div>
+
             <div className={styles.participantcontainer}>
-            <div className={styles.participantitem1}>
-                <span className={styles.name}>Name </span>
-                <span className={styles.email}>Email </span>
-                <span className={styles.type1}>Participant or Volunteer </span>
+                <div className={styles.participantitem1}>
+                    <span className={styles.name}>Name </span>
+                    <span className={styles.email}>Email </span>
+                    <span className={styles.type1}>Participant or Volunteer </span>
+                </div>
             </div>
-            </div>
-            {
+            { 
                 
                 (
-                    activeSearch.length > 0 && 
-                    <div className={styles.participantcontainer}>
+                activeSearch.length > 0 && 
+                <div className={styles.participantcontainer}>
                         {activeSearch.map((participant, index) => (
-                        <div key={index} className={styles.participantitem}>
-                            <span className={styles.name}>{participant.name} </span>
-                            <span className={styles.email}>{participant.email} </span>
-                            <span className={styles.type}>{participant.type}   </span>
-                        </div>
+                    <div key={index} className={styles.participantitem}>
+                        <span className={styles.name}>{participant.first_name}{" "}{participant.last_name} </span>
+                        <span className={styles.email}>{participant.email} </span>
+                        <span className={styles.type}>{participant.type}   </span>
+                    </div>
                          ))}
+                </div>
+                )
+            }
+            {
+                (
+                    showFilterBar && 
+                    <div className={styles.filterbar}>
+                        <div className={styles.parent}>
+                            <input className ={styles.filterbar_search} type="search" placeholder='Last Name' value={lastNameFilter} onChange={(e) => setLastNameFilter(e.target.value)}>
+                            </input>
+                            <input className ={styles.filterbar_search} type="search" placeholder='Town/City' value={townFilter} onChange={(e) => setTownFilter(e.target.value)}>
+                            </input>
+                            <input className ={styles.filterbar_search} type="search" placeholder='Age' value={ageFilter} onChange={(e) => setAgeFilter(e.target.value)}>
+                            </input>
+                            
+                        </div>
+                        <div className={styles.parent_bottom}>
+                            <input className={styles.box} type="checkbox" checked={participantFilter} onChange={() => setParticipantFilter(!participantFilter)}></input> <p className={styles.checkbox}>Participants</p>
+                            <input className={styles.box} type="checkbox" checked={volunteerFilter} onChange={() => setVolunteerFilter(!volunteerFilter)}></input> <p className={styles.checkbox}>Volunteer</p>
+                            <div className={styles.button}>
+                                <button className={styles.apply} onClick={() => applyFilters(participants)}>APPLY</button>
+                            </div>
+                        </div>
                     </div>
                 )
             }
-
-        </form>
+        </div>
+       
     )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/home.module.css";
-import Searchbar from "./admin_dash.tsx"; 
-
+import Searchbar from "@/pages/admin_dash.tsx"
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
