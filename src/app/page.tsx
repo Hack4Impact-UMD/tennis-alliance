e1a31bcc7df079c44d9770b89909ef14a83e418b
Tony Wu <tonywu315@gmail.com>
2023-12-03 20:43:17 -0500

    Remove code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 import Image from "next/image";
 import style from "./page.module.css";
-import { createUser, authenticateUser, logOut, monitorAuthState } from "../api/auth";
+import {
+    createUser,
+    authenticateUser,
+    logOut,
+    monitorAuthState,
+} from "../api/auth";
 import { user } from "../api/test";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 // const user = {
 //     uid: 1101,
@@ -24,69 +29,75 @@ import {useEffect, useState} from "react";
 //   }
 
 const Home = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    
-    const [loggedIn, setLoggedIn] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    
-    
+    const [loggedIn, setLoggedIn] = useState(false);
 
     return (
         <main className={style.main}>
             <div className={style.description}>
-                
                 <div>
-                    
-                    <button
-                        onClick={() => createUser(user)}
-                    >
-                        CLICK ME
-                    </button>
-                    <br/>
+                    <button onClick={() => createUser(user)}>CLICK ME</button>
+                    <br />
                     <form>
                         <div className={style.personal}>
-                        <label htmlFor='email'>Email</label>
-                        <br />
-                        <input
-                            type="text"
-                            id="email"
-                            onChange={(event) => setEmail(event.target.value)}
-                            className={style.textbox}
-                            required
-                        />
-                        <br />
-                        <div className={style.textNames}>
-                            <div className={style.nameTitles}>
-                            <label htmlFor='password'>Password</label>
+                            <label htmlFor="email">Email</label>
                             <br />
                             <input
-                                type="password"
-                                id="password"
-                                onChange={(event) => setPassword(event.target.value)}
-                                className={style.phone}
+                                type="text"
+                                id="email"
+                                onChange={(event) =>
+                                    setEmail(event.target.value)
+                                }
+                                className={style.textbox}
                                 required
                             />
+                            <br />
+                            <div className={style.textNames}>
+                                <div className={style.nameTitles}>
+                                    <label htmlFor="password">Password</label>
+                                    <br />
+                                    <input
+                                        type="password"
+                                        id="password"
+                                        onChange={(event) =>
+                                            setPassword(event.target.value)
+                                        }
+                                        className={style.phone}
+                                        required
+                                    />
+                                </div>
                             </div>
-                        </div>
                         </div>
 
                         <div className={style.submitLocation}>
-                            <button type="button" onClick={() => {authenticateUser(email, password); setLoggedIn(true)}} className={style.submitButton}>
+                            <button
+                                type="button"
+                                onClick={() => {
+                                    authenticateUser(email, password);
+                                    setLoggedIn(true);
+                                }}
+                                className={style.submitButton}
+                            >
                                 Log In
                             </button>
                             <br />
-                            <button type="button" onClick={() => {logOut(); setLoggedIn(false)}} className={style.submitButton}>
+                            <button
+                                type="button"
+                                onClick={() => {
+                                    logOut();
+                                    setLoggedIn(false);
+                                }}
+                                className={style.submitButton}
+                            >
                                 Sign Out
                             </button>
                             <br />
                         </div>
                     </form>
-                    
-                    
                 </div>
             </div>
-
         </main>
     );
 };
