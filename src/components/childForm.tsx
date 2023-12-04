b4cba644bdc72669e64d9a2c4930ff2c71c442db
Tony Wu <tonywu315@gmail.com>
2023-12-03 22:53:48 -0500

    Simplify HTML and CSS

---
+++ src/components/childForm.tsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import Image from "next/image";
+import style from "@/styles/settings.module.css";
+import plus from "@/assets/plus.png";
+
+const ChildForm = () => {
+    const [sections, setSections] = useState([{ id: 1 }]);
+
+    const addSection = () => {
+        const newSections = [...sections, { id: sections.length + 1 }];
+        setSections(newSections);
+    };
+    const removeSection = (idToRemove: number) => {
+        if (sections.length > 1) {
+            const updatedSections = sections.filter(
+                (section) => section.id !== idToRemove
+            );
+            const updatedIDs = updatedSections.map((section, index) => ({
+                ...section,
+                id: index + 1,
+            }));
+            setSections(updatedIDs);
+        }
+    };
+
+    return (
+        <div>
+            {sections.map((section) => (
+                <div key={section.id} className={style.child}>
+                    <div className={style.fields}>
+                        <div>
+                            <label htmlFor={`first_name_${section.id}`}>
+                                First Name
+                            </label>
+                            <input
+                                id={`first_name_${section.id}`}
+                                type="text"
+                                placeholder=""
+                                className={style.individualNames}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor={`last_name_${section.id}`}>
+                                Last Name
+                            </label>
+                            <input
+                                id={`last_name_${section.id}`}
+                                type="text"
+                                placeholder=""
+                                className={style.individualNames}
+                            />
+                        </div>
+                    </div>
+                    <label htmlFor={`email_${section.id}`}>Email</label>
+                    <input id={`email_${section.id}`} type="text" />
+                </div>
+            ))}
+            <div className={style.editChildren}>
+                <div onClick={addSection}>
+                    <label htmlFor="name">Add Child</label>
+                    <Image src={plus} alt="plus" />
+                </div>
+                <div onClick={() => removeSection(sections.length - 1)}>
+                    <label htmlFor="name">Remove Child</label>
+                    <Image src={plus} alt="plus" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ChildForm;
--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,101 +1,94 @@
 import Image from "next/image";
-import React from 'react';
-import style from "../../styles/settings.module.css";
-import tennis from "../../assets/tennispic.png";
-import profile from "../../assets/Vector.png";
-import Children from "./addremove";
+import style from "@/styles/settings.module.css";
+import tennis from "@/assets/tennispic.png";
+import profile from "@/assets/Vector.png";
+import ChildForm from "@/components/childForm";
 
-export default function Settings() {
-
+const Settings = () => {
     return (
-        <div className={style.body}>
-            <div className={style.allDivs}>
-                <form>
-                    <div className={style.top}>
-                        <div className={style.accHead}>
-                            <h3>Account</h3>
-                        </div>
-                        <Image src={tennis} alt="tennis"/>
-                    </div>
-                    
-                    <hr className={style.hhhr}></hr>
-                    
-
-                    <Image src={profile} alt="profile" priority={false}/>
-                    
-                    <h3 className={style.additional}>Full Name</h3>
-                    
-
-                    <div className={style.textNames}>
-                        <div className={style.nameTitles}>
-                            <label htmlFor='name' > First Name </label>
-                            <input type="text" id="first_name" placeholder='' className={style.individualNames} required/>
-                        </div>
-                        <div className={style.nameTitles}>
-                            <label htmlFor='name' > Last Name </label>
-                            <input type="text" id="last_name" placeholder=''   className={style.individualNames} required/>
-                        </div>
-                    </div>  
-                    <hr className={style.hhhr}></hr>
-                    <h3>Personal Info</h3>
-
-                    <div className={style.personal}>
-                        <label htmlFor='name'> Email </label>
-                        <input type="text" id="email" className={style.textbox}/>
-                        <div className={style.textNames}>
-                            <div className={style.nameTitles}>
-                                <label htmlFor='name' > Phone Number </label>
-                                <input type="text" id="phone" placeholder='' className={style.phone} required/>
-                            </div>
-                            <div className={style.numTitles}>
-                                <label htmlFor='name' > Zip Code </label>
-                                <input type="text" id="zip" placeholder=''   className={style.individualNames} required/>
-                            </div>
-                        </div>   
-                    </div>
-
-                    <hr className={style.hhhr}></hr>
-                    <h3>Children Info</h3>
-
-                    <Children/>
-                    
-                    <hr className={style.hhhr}></hr>
-                    <h3>Additional Info</h3>
-
-                    <div className={style.additional}>
-                        <label>Tell us about your background and interests</label> 
-                        <textarea id={style.textarea} rows={10}/> 
-                    </div>
-
-                    <legend>Check any skills that apply to you: </legend>
-                    <div className={style.checkbox}>
-                        <input type="checkbox" id="fundraising" name="skill" value="fundraising" />
-                        <label htmlFor="fundraising">Fundraising</label>
-                    </div>
-                    <div className={style.checkbox}>
-                        <input type="checkbox" id="marketing" name="skill" value="marketing" />
-                        <label htmlFor="marketing">Marketing</label>
-                    </div>
-                    <div className={style.checkbox}>
-                        <input type="checkbox" id="event_planning" name="skill" value="event_planning" />
-                        <label htmlFor="event_planning">Event Planning</label>
-                    </div>
-                    <div className={style.checkbox}>
-                        <input type="checkbox" id="other" name="skill" value="other" />
-                        <label htmlFor="other">Other</label>
-                    </div>
-                    <div>
-                        <label>If other, please specify</label> 
-                        <textarea id={style.textarea} rows={10} required/> 
-                    </div>
-
-                    <div className={style.submitLocation}>
-                        <button className={style.submitButton}>SAVE</button>
-                    </div>
-                    
-
-                </form>
+        <form className={style.container}>
+            <div className={style.top}>
+                <h1>Account</h1>
+                <Image src={tennis} alt="tennis" />
             </div>
-        </div>
+            <hr />
+            <Image src={profile} alt="profile" priority={false} />
+            <h3>Full Name</h3>
+
+            <div className={style.fields}>
+                <div>
+                    <label htmlFor="firstnamename">First Name</label>
+                    <input id="firstname" type="text" placeholder="" required />
+                </div>
+                <div>
+                    <label htmlFor="lastname">Last Name</label>
+                    <input id="lastname" type="text" placeholder="" required />
+                </div>
+            </div>
+            <hr />
+            <h3>Personal Info</h3>
+            <label htmlFor="name">Email</label>
+            <input id="email" type="text" className={style.email} />
+            <div className={style.fields}>
+                <div>
+                    <label htmlFor="name">Phone Number</label>
+                    <input
+                        id="phone"
+                        type="text"
+                        placeholder=""
+                        className={style.phone}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="zip">Zip Code</label>
+                    <input id="zip" type="text" placeholder="" required />
+                </div>
+            </div>
+            <hr />
+            <h3>Children Info</h3>
+            <ChildForm />
+            <hr />
+            <h3>Additional Info</h3>
+            <label>Tell us about your background and interests</label>
+            <textarea rows={10} />
+            <legend>Check any skills that apply to you:</legend>
+            <div className={style.checkbox}>
+                <input
+                    id="fundraising"
+                    type="checkbox"
+                    name="skill"
+                    value="fundraising"
+                />
+                <label htmlFor="fundraising">Fundraising</label>
+            </div>
+            <div className={style.checkbox}>
+                <input
+                    id="marketing"
+                    type="checkbox"
+                    name="skill"
+                    value="marketing"
+                />
+                <label htmlFor="marketing">Marketing</label>
+            </div>
+            <div className={style.checkbox}>
+                <input
+                    id="event_planning"
+                    type="checkbox"
+                    name="skill"
+                    value="event_planning"
+                />
+                <label htmlFor="event_planning">Event Planning</label>
+            </div>
+            <div className={style.checkbox}>
+                <input id="other" type="checkbox" name="skill" value="other" />
+                <label htmlFor="other">Other</label>
+            </div>
+            <label>If other, please specify</label>
+            <textarea rows={10} required />
+            <button>Save</button>
+        </form>
     );
-}
+};
+
+export default Settings;
