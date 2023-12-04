12408bbb4c0e2b4adb0a41d5863291fdca7bdc44
hanockt <91331102+hanockt@users.noreply.github.com>
2023-12-03 21:05:20 -0500

    Auth user (#23)
    * 1st commit user-auth
    
    * added config/firebase.ts
    
    * re implemented cloud function in auth.ts and added functions export in config.ts
    
    * create user push. type error
    
    * still debugging createUser
    
    * edits
    
    * edits
    
    * Fix bug
    
    * Frontend can now interact with backend
    
    * user authentication start
    
    * implemented login/logout
    
    * Finished create user and login functionality
    
    * Remove code
    
    * Refactor
    
    * Polish code
    
    ---------
    
    Co-authored-by: hanock <htsegaye@terpmail.umd.edu>
    Co-authored-by: Tony Wu <tonywu315@gmail.com>

---
+++ src/api/auth.ts
@@ -0,0 +1,97 @@
+import { functions, auth } from "../config";
+
+import { httpsCallable } from "firebase/functions";
+import { type AuthError } from "@firebase/auth";
+import {
+    signInWithEmailAndPassword,
+    // getAuth,
+    signOut,
+    // sendPasswordResetEmail,
+    UserCredential,
+    onAuthStateChanged,
+} from "firebase/auth";
+
+type Children = {
+    firstName: string;
+    lastName: string;
+    age: number;
+    birthYear: number;
+    school: string;
+};
+
+type User = {
+    uid: number;
+    newEmail: string;
+    password: string;
+    newFirstName: string;
+    newLastName: string;
+    phoneNumber: number;
+    zipCode: number;
+    children: Children[];
+    notifcations: boolean;
+};
+
+// Create user and logs them in
+export function createUser(user: User): Promise<void> {
+    return new Promise((resolve, reject) => {
+        const createUserCloudFunction = httpsCallable(functions, "createUser");
+        createUserCloudFunction(user)
+            .then(() => {
+                console.log(user);
+                // Logs user after creating account
+                authenticateUser(user.newEmail, user.password);
+                resolve();
+            })
+            .catch((error: string) => {
+                console.log(user);
+                reject(error);
+            });
+    });
+}
+
+// Log in
+export function authenticateUser(
+    email: string,
+    password: string
+): Promise<UserCredential> {
+    return new Promise((resolve, reject) => {
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                console.log(userCredential.user);
+                monitorAuthState();
+                resolve(userCredential);
+            })
+            .catch((error: AuthError) => {
+                reject(error);
+            });
+    });
+}
+
+export const monitorAuthState = () => {
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            // Needs to implement transition to user dashboard IF Just logged in
+            // I think it shouldn't do anything if already logged in
+            console.log(user);
+            console.log("logged in! switch to user dashboard page");
+        } else {
+            // If not logged in, current page should be the login page
+            console.log("you are not logged in");
+        }
+    });
+};
+
+// Log out
+export function logOut(): Promise<void> {
+    return new Promise((resolve, reject) => {
+        signOut(auth)
+            .then(() => {
+                console.log("logged out!");
+                monitorAuthState();
+                resolve();
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+}
