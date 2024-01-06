087298e5f444a70562d18bb2451ba10096121256
Tony Wu <tonywu315@gmail.com>
2024-01-06 00:53:58 -0500

    Clean up code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ const Document = () => {
     return (
         <Html lang="en">
             <Head />
-            <body style={{ marginTop: "100px" }}>
+            <body>
                 <Main />
                 <NextScript />
             </body>
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,66 +1,94 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Image from "next/image";
 // import { createUser, authenticateUser, logOut } from "@/api/auth";
-import ForgotPasswordPopup from "@/components/forgotPasswordPopup";
-import styles from "@/styles/page.module.css";
+import styles from "@/styles/login.module.css";
 import miniLogo from "@/assets/mini_logo.png";
-import tennisBalls from "@/assets/TennisBalls.png";
+import tennisBalls from "@/assets/tennis_balls.png";
+import circleX from "@/assets/circle_x.png";
 
 const LoginPage = () => {
+    const ref = useRef(null);
+    const [forgotPassword, setForgotPassword] = useState(false);
+    const [width, setWidth] = useState(0);
+    const [height, setHeight] = useState(0);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [showPopup, setShowPopup] = useState(false);
+
+    useEffect(() => {
+        setWidth(ref.current?.clientWidth | 0);
+        setHeight(ref.current?.clientHeight | 0);
+    }, [ref]);
 
     return (
-        <div className={styles.background}>
-            <Image className={styles.miniLogo} src={miniLogo} alt="miniLogo" />
-            <form className={styles.loginForm}>
-                <p className={styles.email}>Email</p>
-                <input
-                    id="email"
-                    type="text"
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-                <p className={styles.pw}>Password</p>
-                <input
-                    id="password"
-                    type="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-
-                <a
-                    href="#"
-                    className={styles.forgotPasswordLink}
-                    onClick={() => setShowPopup(true)}
+        <div className={styles.container}>
+            {forgotPassword ? (
+                <form
+                    className={`${styles.form} ${styles.forgot}`}
+                    style={{ width: width, height: height }}
                 >
-                    Forgot password?
-                </a>
-
-                {showPopup && (
-                    <ForgotPasswordPopup onClose={() => setShowPopup(false)} />
-                )}
-
-                <button type="submit" className={styles.button}>
+                    <Image src={miniLogo} alt="mini logo" />
                     <Image
-                        className={styles.tennisBalls}
-                        src={tennisBalls}
-                        alt="tennisBalls"
+                        className={styles.x}
+                        src={circleX}
+                        alt="x button"
+                        onClick={() => setForgotPassword(false)}
                     />
-                    Submit
-                </button>
-
-                <span className={styles.bottomText}>
-                    <a href="#" className={styles.signUpLink}>
-                        Sign up
-                    </a>
-                    for an account/One time event
-                    <a href="#" className={styles.signUpLink}>
-                        sign up
-                    </a>
-                </span>
-            </form>
+                    <p>Email</p>
+                    <input
+                        id="email"
+                        type="email"
+                        placeholder="Enter your email address"
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                    <button type="submit">
+                        <Image
+                            className={styles.tennisBalls}
+                            src={tennisBalls}
+                            alt="tennisBalls"
+                        />
+                        Submit
+                    </button>
+                </form>
+            ) : (
+                <form className={styles.form} ref={ref}>
+                    <Image src={miniLogo} alt="miniLogo" />
+                    <p>Email</p>
+                    <input
+                        id="email"
+                        type="email"
+                        placeholder="Email address"
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                    <div className={styles.password}>
+                        <p>Password</p>
+                        <span onClick={() => setForgotPassword(true)}>
+                            Forgot password?
+                        </span>
+                    </div>
+                    <input
+                        id="password"
+                        type="password"
+                        placeholder="Password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                    <button type="submit">
+                        <Image
+                            className={styles.tennisBalls}
+                            src={tennisBalls}
+                            alt="tennisBalls"
+                        />
+                        Submit
+                    </button>
+                    <span>
+                        <a className={styles.signUpLink}>Sign up</a> for an
+                        account/One time event{" "}
+                        <a className={styles.signUpLink}>sign up</a>
+                    </span>
+                </form>
+            )}
         </div>
     );
 };
