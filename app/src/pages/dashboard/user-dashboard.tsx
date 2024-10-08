a224d79018772a25cffb260f077be5fbab0c06d0
Rivan Parikh <rivan.parikh@gmail.com>
2024-10-07 23:28:53 -0400

    updated file structure

---
+++ app/src/pages/dashboard/user-dashboard.tsx
@@ -0,0 +1,12 @@
+import MyCalendar from '../../components/calendar-main';
+
+const App: React.FC = () => {
+    return (
+      <div>
+        <h1>My Calendar</h1>
+        <MyCalendar />
+      </div>
+    );
+  };
+  
+  export default App;
